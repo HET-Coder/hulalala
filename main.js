45865338340e922a0d0c43301bd990bad771e407
HET-Coder <95082442+HET-Coder@users.noreply.github.com>
2022-07-08 17:56:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is initialized');
+}
+
+function draw() {
+    background('#71AFE2');
+
+    document.getElementById("square_side").innerHTML = "Width and Height of a square will be = " + difference + "px";
+    fill("black");
+    stroke("gray");
+    square(noseX, noseY, difference);
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.x;
+        noseY = results[0].pose.y;
+        console.log("noseX =" + noseX + "noseY = " +noseY);
+
+        leftWristX = results[0].pose.leftWrist.x
+        rightWristX = results[0].pose.rightWrist.x
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX =" + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
